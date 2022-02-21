dea3271c3677f79f54922851980e04c4dee4da9f
Mirnula <95304071+Mirnula@users.noreply.github.com>
2022-02-21 22:54:44 +0100

    calling api for the current temp

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-let apiKey = "0163abee892c1733e53a43b6a0e7908a";
+let apiKey = `0163abee892c1733e53a43b6a0e7908a`;
+let apiBeggining = `https://api.openweathermap.org/data/2.5/weather?`;
+let unit = `metric`;
+let apiUrl = `${apiBeggining}q=New York&units=${unit}&appid=${apiKey}`;
+
+function displayTemperature(response) {
+  let temperatureNumber = document.querySelector("#temperature");
+  temperatureNumber.innerHTML = Math.round(response.data.main.temp);
+  let cityName = document.querySelector("#city");
+  cityName.innerHTML = response.data.name;
+  let weatherDescription = document.querySelector(".weather-description");
+  weatherDescription.innerHTML = response.data.weather[0].main;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.main.humidity;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
+}
+
+axios.get(apiUrl).then(displayTemperature);
