6fcd7a111b7f7c101f05ce252c6733b324c13d08
Mirnula <95304071+Mirnula@users.noreply.github.com>
2022-02-27 19:46:28 +0100

    connected new API to the days of the week

--- src/app.js
+++ src/app.js
@@ -16,6 +16,52 @@ let todayDate = document.querySelector(".date-time");
 
 todayDate.innerHTML = ` ${day}, ${hour}:${minutes} `;
 
+function formatDay(time) {
+  let date = new Date(time * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = date.getDay();
+  return days[day];
+}
+
+function displayWeekWeather(response) {
+  let daysOfWeek = response.data.daily;
+  let weeklyWeather = document.querySelector(".days-of-week");
+
+  let forecastHTML = `<div class="row">`;
+
+  daysOfWeek.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `  <div class="col-2">
+       <div class="day-forecast">${formatDay(forecastDay.dt)}</div>
+       <img
+       src="http://openweathermap.org/img/wn/${
+         forecastDay.weather[0].icon
+       }@2x.png"
+       class="image-weather"
+       width="40"
+       />
+     <div class="temperature-forecast">
+    <span class="max-temp"> ${Math.round(forecastDay.temp.max)}°</span>
+    <span class="min-temp">${Math.round(forecastDay.temp.min)}°</span>
+    </div>
+     </div>
+  `;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+
+  weeklyWeather.innerHTML = forecastHTML;
+}
+function getWeeklyForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = `0163abee892c1733e53a43b6a0e7908a`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayWeekWeather);
+}
+
 function displayTemperature(response) {
   let temperatureNumber = document.querySelector(".temperature");
   temperatureNumber.innerHTML = Math.round(response.data.main.temp);
@@ -32,6 +78,8 @@ function displayTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getWeeklyForecast(response.data.coord);
 }
 
 function search(city) {
